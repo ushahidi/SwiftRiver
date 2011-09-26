6358b1a5577da9e679434cdd522cc309596f7392
David Kobia <david@ushahidi.com>
2011-09-26 12:12:57 -0400

    * Linking to stories and streams from projects page

--- themes/default/views/pages/projects/overview.php
+++ themes/default/views/pages/projects/overview.php
@@ -22,8 +22,8 @@
 		?>
 			<tr <?php if ($i == 0) { echo 'class="bg"'; } ?>>
 				<td><h4><a href="<?php echo URL::site('/project/')."/".$project->id; ?>"><?php echo $project->project_title; ?></a></h4></td>
-				<td><?php echo $stories; ?></td>
-				<td><?php echo $items; ?></td>
+				<td><h4><a href="<?php echo URL::site('/project/')."/".$project->id."/stories"; ?>"><?php echo $stories; ?></a></h4></td>
+				<td><h4><a href="<?php echo URL::site('/project/')."/".$project->id."/stream"; ?>"><?php echo $items; ?></a></h4></td>
 				<td><a href="<?php echo URL::site('/project/')."/".$project->id; ?>"><img src="<?php echo URL::base();?>themes/default/media/img/ico-show.gif" class="ico" alt="Show" /></a>  <a href="<?php echo URL::site('/projects/edit/')."/".$project->id; ?>"><img src="<?php echo URL::base();?>themes/default/media/img/ico-edit.gif" class="ico" alt="Edit" /></a>  <a href="#"><img src="<?php echo URL::base();?>themes/default/media/img/ico-delete.gif" class="ico" alt="Delete" /></a></td>
 			</tr>
 		<?php
