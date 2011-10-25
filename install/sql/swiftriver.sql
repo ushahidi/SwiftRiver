be0b973eabf6fba83ac99099fc00d70c47196088
David Kobia <david@ushahidi.com>
2011-10-25 17:17:39 -0400

    Removed more code errors in sql file

--- install/sql/swiftriver.sql
+++ install/sql/swiftriver.sql
@@ -464,9 +464,6 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 -- Data for table `roles`
 -- -----------------------------------------------------
