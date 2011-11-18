e6de6b0fa3e82673f60202d7237b494185d72d84
Emmanuel Kala <emmanuel@ushahidi.com>
2011-11-18 20:28:43 +0300

    * IMAP library for the email plugin and accompanying unit test. Like a boss! :punch:
    * Unit test for the IMAP library needs to be seeded with proper/valid
    parameters

--- application/tests/swiftriver/SwiftcoreTest.php
+++ application/tests/swiftriver/SwiftcoreTest.php
@@ -2,10 +2,11 @@
 /**
  * Unit test for the Swiftcore plugin
  *
- * @author Ushahidi Team
- * @package Swiftriver
- * @category Unit Tests
- * @copyright (c) 2008-2011 Ushahidi Inc <http://www.ushahidi.com>
+ * @package    Swiftriver
+ * @category   Tests
+ * @author     Ushahidi Team
+ * @copyright  (c) 2008-2011 Ushahidi Inc <http://www.ushahidi.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License v3 (GPLv3)
  */
 class Swiftriver_Swiftcore_Test extends Unittest_TestCase {
 	
@@ -39,13 +40,19 @@ class Swiftriver_Swiftcore_Test extends Unittest_TestCase {
 	{
 		$this->assertTrue(Swiftriver_Event::has_run('swiftriver.droplet.extract_metadata'), 'Event has not run');
 		
+		// Get the droplet
 		$this->dropplet = Swiftriver_Event::$data;
+		
 		// Check that tags have been set
 		$this->assertGreaterThan(0, count($this->droplet['tags']), "Entity extraction failed");
 	}
 	
+	/**
+	 * Overrides default tearDown
+	 */
 	public function tearDown()
 	{
+		// Garbage collection
 		unset ($this->droplet);
 	}
 	
