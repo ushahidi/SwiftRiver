e3421eb4e6035360210ec63cc2b4a6de04e6a884
David Kobia <david@ushahidi.com>
2011-12-05 14:47:27 -0500

    Settings and Droplets
    Initial commit for settings/plugins. Initial commit for droplets.

--- themes/default/views/pages/river/main.php
+++ themes/default/views/pages/river/main.php
@@ -3,17 +3,12 @@
 		<hgroup class="edit">
 			<h1><span class="edit_trigger" title="river" id="edit_<?php echo $river->id; ?>" onclick=""><?php echo $river->river_name; ?></span></h1>
 		</hgroup>
-		<?php
-		if (isset($droplets))
-		{
-			?>
+		<?php if ( count($droplets) ):?>
 			<section class="meter">
-				<p style="padding-left:<?php echo $meter; ?>%;"><strong><?php echo $droplets; ?></strong> <?php echo __('Droplets'); ?></p>
+				<p style="padding-left:<?php echo $meter; ?>%;"><strong><?php echo $filtered_total; ?></strong> <?php echo __('Droplets'); ?></p>
 				<div><span style="width:<?php echo $meter; ?>%;"></span></div>
 			</section>		
-			<?php
-		}
-		?>
+		<?php endif; ?>
 	</div>
 	
 	<div class="center canvas">
@@ -25,19 +20,59 @@
 					<li><a href="#">Tags</a></li>
 					<li><a href="#">Links</a></li>
 					<li><a href="#">Sources</a></li>
-					<li class="view_panel"><a href="<?php echo $more; ?>"><span class="arrow"></span>More</a></li>
+					<li class="view_panel"><a href="<?php echo $more_url; ?>"><span class="arrow"></span>More</a></li>
 				</ul>
 				<ul class="actions">
-					<li class="view_panel"><a href="<?php echo $filters; ?>" class="channels"><span class="icon"></span><?php echo __('Edit Filter'); ?></a></li>
-					<li class="view_panel"><a href="<?php echo $settings; ?>" class="filter"><span class="icon"></span><?php echo __('River Settings'); ?></a></li>
+					<li class="view_panel"><a href="<?php echo $filters_url; ?>" class="channels"><span class="icon"></span><?php echo __('Edit Filter'); ?></a></li>
+					<li class="view_panel"><a href="<?php echo $settings_url; ?>" class="filter"><span class="icon"></span><?php echo __('River Settings'); ?></a></li>
 				</ul>
 			</nav>
 			<div class="panel_body"></div>
 		</section>
 
-		<?php
+		<div class="trend_container cf" id="river_droplets">
+			<?php foreach ($droplets as $droplet): ?>
+				<article class="droplet cf">
+					<div class="summary">
+						<section class="source twitter">
+							<a href="/user"><img src="/images/content/avatar1.gif" /></a>
+							<div class="actions">
+								<span class="type"></span>
+								<p class="button_change score"><a onclick=""><span>45</span></a><p>
+								<div class="clear"></div>
+								<ul class="dropdown left">
+									<li class="confirm"><a onclick="">This is useful</a></li>
+									<li class="not_useful"><a onclick="">This is not useful</a></li>
+								</ul>
+							</div>
+						</section>
+						<section class="content">
+							<div class="title">
+								<p class="date">July 4, 2011</p>
+								<h1>Adam Tinworth</h1>
+							</div>
+							<div class="body">
+								<p>OK, the Ushahidi section of this afternoon's #likeminds post should now be more link rich and comprehensible: <a href="#">t.co/D2lk9lRg</a></p>
+							</div>
+						</section>
+						<section class="actions">
+							<p class="button_view"><a href="/droplet/detailA.html" class="detail_view"><span></span><strong>detail</strong></a></p>
+							<div class="button">
+								<p class="button_change bucket"><a><span></span><strong>buckets</strong></a></p>
+								<div class="clear"></div>
+								<ul class="dropdown">
+									<li class="bucket"><a onclick=""><span class="select"></span>Bucket 1</a></li>
+									<li class="bucket"><a onclick=""><span class="select"></span>Bucket 2</a></li>
+									<li class="create_new"><a onclick=""><span class="create_trigger"><em>Create new</em></span></a></li>
+								</ul>
+							</div>
+						</section>
+					</div>
+					<section class="detail cf"></section>
+				</article>
+			<?php endforeach; ?>
+		</div>
 
-		?>
 		<div class="page_buttons">
 		<p class="button_view"><a href="#">View more</a></p>
 		</div>
--- themes/default/views/pages/settings/main.php
+++ themes/default/views/pages/settings/main.php
@@ -1,5 +1,24 @@
-<div>
+<?php echo Form::open(); ?>
+	<div id="settings">
+		<div id="messages"></div>
 
-xxxx
+		<div class="controls">
+			<div class="row cf">
+				<div class="input">
+					<h3><?php echo __('Site Name'); ?></h3>
+					<?php echo Form::input("site_name", ''); ?>
+				</div>
+			</div>
+			<div class="row cf">
+				<div class="input">
+					<h3><?php echo __('Site Locale'); ?></h3>
+					<?php echo Form::password("site_locale", ''); ?>
+				</div>
+			</div>
 
-</div>
+		<div class="row controls_buttons cf">
+			<p class="button_go"><a href="#"><?php echo __('Apply changes'); ?></a></p>
+			<p class="other"><a class="close" onclick=""><?php echo __('Cancel'); ?></a></p>
+		</div>
+	</div>
+<?php echo Form::close(); ?>
