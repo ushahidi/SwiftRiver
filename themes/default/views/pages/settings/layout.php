f2effac8175d8372d0154c9ac663656b587a6c02
Emmanuel Kala <emmanuel@ushahidi.com>
2012-02-07 09:53:43 +0300

    Cleanup for the plugin listing UI

--- themes/default/views/pages/settings/layout.php
+++ themes/default/views/pages/settings/layout.php
@@ -1,4 +1,4 @@
-<article id="droplet-full" class="droplet dashboard cf">
+<article id="droplet-full" class="list dashboard cf">
 	<div class="cf center page-title">
 		<h1><span><?php echo __('Settings') ?></span></h1>
 	</div>
@@ -7,8 +7,12 @@
 		<section class="panel">		
 			<nav class="cf">
 				<ul class="views">
-					<li <?php if ($active == 'main' OR ! $active) echo 'class="active"'; ?>><a href="<?php echo URL::site().'settings';?>"><?php echo __('Application'); ?></a></li>
-					<li <?php if ($active == 'plugins') echo 'class="active"'; ?>><a href="<?php echo URL::site().'settings/main/plugins';?>"><?php echo __('Plugins'); ?></a></li>
+					<li <?php if ($active == 'main' OR ! $active) echo 'class="active"'; ?>>
+						<a href="<?php echo URL::site().'settings';?>"><?php echo __('Application'); ?></a>
+					</li>
+					<li <?php if ($active == 'plugins') echo 'class="active"'; ?>>
+						<a href="<?php echo URL::site().'settings/main/plugins';?>"><?php echo __('Plugins'); ?></a>
+					</li>
 					<?php
 					// Swiftriver Plugin Hook -- add settings nav item
 					Swiftriver_Event::run('swiftriver.settings.nav');
--- themes/default/views/pages/settings/plugins.php
+++ themes/default/views/pages/settings/plugins.php
@@ -1,40 +1,44 @@
 <?php echo Form::open(); ?>
 	<input type="hidden" name="action" value="">
 	<input type="hidden" name="id" value="">
-	<div class="list-stream">
-		<ul>
-			<?php if ($plugins->count()):
-				$i = 0;
-				foreach ($plugins as $plugin)
-				{
-					?>
-					<li id="item_<?php echo $plugin->id; ?>" <?php if($i == 0) echo ' class="alt_row"'; ?>>
-						<a href="#" class="title"><?php echo $plugin->plugin_name; ?></a>
+	<div class="container list select data">
+	<?php if ($plugins->count()): ?>
+		<?php
+			$i = 0;
+			foreach ($plugins as $plugin):
+		?>
+			<article class="item cf <?php if($i == 0) echo "alt_row"; ?>" id="item_<?php echo $plugin->id; ?>">
+				<div class="content">
+					<hgroup>
+						<h3><a href="#" class="title"><?php echo $plugin->plugin_name; ?></a></h3>
 						<span class="description"><?php echo $plugin->plugin_description; ?></span>
-						<div class="actions">
-							<?php if ($plugin->plugin_enabled): ?>
-								<span class="button-delete active"><a><?php echo __('Deactivate Plugin'); ?></a></span>
-								<ul class="dropdown">
-									<p><?php echo __('Are you sure you want to deactivate this Plugin?'); ?></p>
-									<li class="confirm"><a onclick="pluginAction(this, 0,<?php echo $plugin->id; ?>)"><?php echo __('Yep.'); ?></a></li>
-									<li class="cancel"><a onclick=""><?php echo __('No, nevermind.'); ?></a></li>
-								</ul>
-							<?php else: ?>
-								<span class="button-delete"><a><?php echo __('Activate Plugin'); ?></a></span>
-								<ul class="dropdown">
-									<p><?php echo __('Are you sure you want to activate this Plugin?'); ?></p>
-									<li class="confirm"><a onclick="pluginAction(this, 1,<?php echo $plugin->id; ?>)"><?php echo __('Yep.'); ?></a></li>
-									<li class="cancel"><a onclick=""><?php echo __('No, nevermind.'); ?></a></li>
-								</ul>
-							<?php endif; ?>
-						</div>
-					</li>
-				<?php
-					$i = ($i == 0) ? 1 : 0;
-				} 
-			else:?>
-				<li><?php echo __('No Plugins in the System'); ?></li>
-			<?php endif; ?>
-		</ul>
+					</hgroup>
+				</div>
+				<div class="summary">
+					<section class="actions">
+						<?php if ($plugin->plugin_enabled): ?>
+							<p class="button-delete"><a><?php echo __('Deactivate Plugin'); ?></a></p>
+							<ul class="dropdown">
+								<p><?php echo __('Are you sure you want to deactivate this Plugin?'); ?></p>
+								<li class="confirm"><a onclick="pluginAction(this, 0,<?php echo $plugin->id; ?>)"><?php echo __('Yep.'); ?></a></li>
+								<li class="cancel"><a onclick=""><?php echo __('No, nevermind.'); ?></a></li>
+							</ul>
+						<?php else: ?>
+							<p class="button-delete button-delete-subtle"><a><?php echo __('Activate Plugin'); ?></a></p>
+							<ul class="dropdown">
+								<p><?php echo __('Are you sure you want to activate this Plugin?'); ?></p>
+								<li class="confirm"><a onclick="pluginAction(this, 1,<?php echo $plugin->id; ?>)"><?php echo __('Yep.'); ?></a></li>
+								<li class="cancel"><a onclick=""><?php echo __('No, nevermind.'); ?></a></li>
+							</ul>
+						<?php endif; ?>
+					</section>
+				</div>
+			</article>
+			<?php $i = ($i == 0) ? 1 : 0; ?>
+		<?php endforeach; ?>
+		
+	<?php else:?>
+		<li><?php echo __('No Plugins in the System'); ?></li>
+	<?php endif; ?>
 	</div>
 <?php echo Form::close(); ?> 
