23bdcd87f1c086ea64a75c826afb3173ec44d0dc
Nick <silent.github@transparentage.com>
2012-06-18 21:04:48 +0300

    Plugins/FeedWriter: Added RSS button through JQuery injection.

--- plugins/feedwriter/init.php
+++ plugins/feedwriter/init.php
@@ -9,6 +9,22 @@
  * @copyright (c) 2008-2012 Ushahidi Inc <http://ushahidi.com>
  */
 
+class Feedwriter_Init
+{
+    public function __construct()
+    {
+        Swiftriver_Event::add('swiftriver.template.head', array('Feedwriter_Init', 'Inject'));
+    }
+
+    public static function Inject()
+    {
+        echo '<script type="text/javascript">$(document).ready(function(){$(".page-title.bucket-title.cf div h1 span").after(\'<span class="rss-feed" style="margin-left: 20px; display: none;"><a href="rss" target="_blank"><img src="/themes/default/media/img/channel-rss.gif" alt="RSS" title="RSS" height="12" style="margin-bottom: 4px;" /></a></span>\');$("span.rss-feed").fadeIn()})</script>';
+    }
+}
+
+new Feedwriter_Init;
+
+// Bind the plugin to valid URLs
 Route::set('feeds', '<account>/bucket/<name>/<action>',
     array(
         'action' => '(rss|atom)' // atom NYI
@@ -17,5 +33,7 @@ Route::set('feeds', '<account>/bucket/<name>/<action>',
         'controller' => 'feedwriter',
         'action' => 'generate'
     ));
+
+
 
 ?>
