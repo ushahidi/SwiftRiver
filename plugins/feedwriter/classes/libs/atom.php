8282d76325da569b4051438377414dd97f3d41a3
Nick <silent.github@transparentage.com>
2012-06-19 13:56:53 +0300

    Plugins/FeedWriter: Missed the header info and die at the top:

--- plugins/feedwriter/classes/libs/atom.php
+++ plugins/feedwriter/classes/libs/atom.php
@@ -1,4 +1,13 @@
-<?php
+<?php defined('SYSPATH') OR die('No direct script access');
+
+/**
+ * Atom-Lib for the FeedWriter plugin
+ *
+ * @package   SwiftRiver
+ * @author    Ushahidi Team
+ * @category  Plugins
+ * @copyright (c) 2008-2012 Ushahidi Inc <http://ushahidi.com>
+ */
 
 class Feedwriter_Atom
 {
