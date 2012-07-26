0ff6c75a87fbc093aa32fb63f551c32db3415973
Brian Muita <bgm@69mb.org>
2012-07-26 10:03:35 +0300

    Fix collaboration levels default value

--- install/sql/updates/2012_07_24_00_bucket_collaborators.sql
+++ install/sql/updates/2012_07_24_00_bucket_collaborators.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `bucket_collaborators` ADD `read_only` TINYINT(1)  NULL  DEFAULT NULL;
+ALTER TABLE `bucket_collaborators` ADD `read_only` TINYINT(1)  NULL  DEFAULT '0';
--- install/sql/updates/2012_07_24_00_river_collaborators.sql
+++ install/sql/updates/2012_07_24_00_river_collaborators.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `river_collaborators` ADD `read_only` TINYINT(1)  NULL  DEFAULT NULL;
+ALTER TABLE `river_collaborators` ADD `read_only` TINYINT(1)  NULL  DEFAULT '0';
