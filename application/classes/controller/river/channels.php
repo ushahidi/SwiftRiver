ushahidi/SwiftRiver

commit 26134e23e2dd2642d8500152c0919fa5ade69703

Improve twitter channel settings - Closes #136
* Validate Twitter users before saving (via the Twitter API)

* Instructions for adding multiple users in the placeholder

* Replace text field with plain text when a channel option is saved

* Namespace the channel option exception as per Kohana's HMVC -
prevents "unknown class name" errors from being thrown

* Send 'delete' message to the MQ when a parameter is modified

application/classes/controller/river/channels.php
@@ -120,7 +120,7 @@ class Controller_River_Channels extends Controller_River_Settings {
 					$option_array = $this->river->get_channel_options($channel, $channel_option->id);
 					echo(json_encode($option_array[0]));
 				}
-				catch (SwiftRiver_Exception_ChannelOption $e)
+				catch (Swiftriver_Exception_Channel_Option $e)
 				{
 					$this->response->status(400);
 					$this->response->headers('Content-Type', 'application/json');

themes/default/views/pages/river/settings/channels.php
@@ -53,8 +53,12 @@
 			<p class="title"><%= title.substring(0, 19) + (title.length > 20 ? "..." : "") %></p>
 		<% } %>
 		<%= input %>
-		<p class="remove-small actions"><span class="icon"></span><span class="nodisplay">Remove</span></p>
+		<p class="remove-small actions">
+			<span class="icon"></span><span class="nodisplay">Remove</span>
+		</p>
 	</label>
+	<div style="clear: both"></div>
+
 </script>
 
 <script type="text/template" id="channel-template">
@@ -247,12 +251,17 @@ $(function() {
 		events: {
 			"click .remove-small": "remove",
 			"focusout input": "hideSaveButton",
-			"click .button-blue a": "save",
+			"click .button-blue a.save": "save",
 			"keyup input": "keypressSave",
+			"click .button-blue a.edit": "edit",
+			"click .button-blue a.cancel": "cancel",
 		},
 		
 		initialize: function(options) {
 			this.config = channelsConfig.getChannelOptionConfig(options.channel.get("channel"), this.model.get('key'));
+			this.renderMode = "view";
+			this.savingMode = false;
+			this.actionsHTML = null;
 		},
 		
 		// Depending on the option type, return a <input>, <select> or group of these
@@ -266,7 +275,8 @@ $(function() {
 				value = ""
 			}
 			
-			var input = "";			
+			var input = "";
+			this.actionsHTML = "";
 			if (config.type == "select") {
 				input = '<select name="' + config.key + '">'
 				input += '<option value="">Select one</option>';
@@ -279,17 +289,26 @@ $(function() {
 					input += '<label><p class="field">' + conf.label + '</p>'
 					input += this.createInputHtml(conf, "", true);
 					input += '</label>';
-				}, this)
-				input += '<span class="button-blue"><a href="#">Save</a></span>';
+				}, this);
+				input += '<span class="button-blue"><a class="save" href="#">Save</a></span>';
 			} else if (config.type == "file") {
 				input = '<span class="button-blue has_file"><a href="#">Select file</a><input type="file" name="file"></span>';
 			} else {
-				input = '<input type="text" name="' + config.key + '" placeholder="' + placeholder + '" value="' + value + '" />';
+				if (value != "" && this.renderMode == "view") {
+					input = '<p class="field-text">'+value+'</p>';
+					this.actionsHTML += '<p class="actions"><span class="button-blue"><a class="edit" href="#">Edit</a></span></p>';
+				} else {
+					input = '<input type="text" name="' + config.key + '" placeholder="' + placeholder + '" value="' + value + '" />';
+					if (value != "" && !group) {
+						this.actionsHTML += '<p class="actions"><span class="button-blue"><a class="cancel"href="#">Cancel</a></span></p>';
+					}
+				}
+
 				if (!group) {
-					input += '<span class="button-blue" style="display:none"><a href="#">Save</a></span>';
+					this.actionsHTML += '<p class="actions" style="display:none"><span class="button-blue"><a class="save" href="#">Save</a></span></p>';
 				}
 			}
-			return input
+			return input;
 		},
 		
 		getInputField: function() {
@@ -301,6 +320,9 @@ $(function() {
 			data.input = this.getInputField();
 			data.config = this.config;
 			this.$el.html(this.template(data));
+			if (this.actionsHTML != null && this.actionsHTML != "") {
+				this.$("p.remove-small").after(this.actionsHTML);
+			}
 			
 			// Attach the file upload handler
 			if (this.config.type == 'file')
@@ -328,13 +350,13 @@ $(function() {
 		
 		showSaveButton: function() {
 			this.$("span.error-message").remove();
-			this.$(".button-blue").fadeIn("slow");
+			this.$("a.save").parents("p.actions").fadeIn("slow");
 		},
 		
 		hideSaveButton: function() {
 			var newValue = this.$("input[type=text]").val();
 			if( ! newValue || newValue == this.model.get("value")  ) {
-				this.$(".button-blue").fadeOut();
+				this.$("a.save").parents("p.actions").fadeOut();
 			}
 		},
 		
@@ -343,8 +365,8 @@ $(function() {
 				this.save();
 				return false;
 			} else {
-				var newValue = this.$("input[type=text]").val();
-				if(newValue && newValue != this.model.get("value") && newValue != "" ) {
+				var newValue = $.trim(this.$("input[type=text]").val());
+				if(newValue != "" && newValue && newValue != this.model.get("value")) {
 					this.showSaveButton();
 				} else {
 					this.hideSaveButton();
@@ -366,13 +388,13 @@ $(function() {
 					// Disable the inputs and show the loading icon
 					view.$("span.error-message").remove();
 					view.$("input").attr("disabled", "disabled").blur();
-					view.$(".button-blue").hide().after(loading_msg);
+					view.$("a.save").parents("p.actions").hide().after(loading_msg);
 				},
 				done: function (e, data) {
 					if (!data.result.length) {
 						var error_msg = $('<span class="error-message">No parameters were found in the file</span>');
 						loading_msg.replaceWith(error_msg).remove();
-						view.$(".button-blue").fadeIn()
+						view.$("a.save").parents("p.actions").fadeIn();
 						view.$("input").removeAttr("disabled");
 						return;
 					} else {
@@ -388,7 +410,7 @@ $(function() {
 					} 
 					var error_msg = $('<span class="error-message">' + message + '</span>');
 					loading_msg.replaceWith(error_msg).remove();
-					view.$(".button-blue").fadeIn()
+					view.$("a.save").parents("p.actions").fadeIn()
 					view.$("input").removeAttr("disabled");
 				}
 			});
@@ -399,7 +421,7 @@ $(function() {
 			// Disable the inputs and show the loading icon
 			this.$("input, select").attr("disabled", "disabled").blur();
 			var loading_msg = window.loading_image.clone();
-			this.$(".button-blue").hide().after(loading_msg);
+			this.$("a.save").parents("p.actions").hide().after(loading_msg);
 			
 			// Get the user's inputs and do grouping for group options
 			var inputs = this.$("input, select");
@@ -414,6 +436,7 @@ $(function() {
 			}
 			
 			// Do the save showing a sucess/error message
+			this.savingMode = true;
 			var view = this;
 			this.model.save({'value': value}, {
 				wait: true,
@@ -425,6 +448,7 @@ $(function() {
 					loading_msg.replaceWith(success_msg).remove();					
 					success_msg.fadeOut(4000, function() {
 						$(this).remove();
+						view.renderMode = "view";
 						view.render();
 					});
 				},
@@ -439,6 +463,24 @@ $(function() {
 				}
 			});
 			
+			this.savingMode = false;
+			return false;
+		},
+
+		// Create edit fields for the channel option
+		edit: function() {
+			this.renderMode = "edit";
+			this.render();
+			return false;
+		},
+
+		// When the edit operation is cancelled
+		cancel: function() {
+			// Only process cancel action when not in saving mode
+			if (!this.savingMode) {
+				this.renderMode = "view";
+				this.render();
+			}
 			return false;
 		}
 	});

application/classes/model/channel/filter.php
@@ -184,6 +184,10 @@ class Model_Channel_Filter extends ORM {
 	public  function update_option($value, $id = 0)
 	{
 		$filter_option = ORM::factory('channel_filter_option', $id);
+		if ($filter_option->loaded())
+		{
+			Swiftriver_Event::run('swiftriver.channel.option.pre_delete', $filter_option);
+		}
 		$filter_option->channel_filter_id = $this->id;
 		$filter_option->key = $value['key'];		
 		unset($value['key']);

application/classes/swiftriver/exception/channel/option.php
@@ -13,5 +13,5 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License v3 (GPLv3) 
  */
 
-class Swiftriver_Exception_ChannelOption extends Kohana_Exception {
+class Swiftriver_Exception_Channel_Option extends Kohana_Exception {
 }

plugins/rss/init.php
@@ -98,7 +98,7 @@ class Rss_Init {
 			$url = $option_data['value'];
 
 			if ( ! ($feed = RSS_Util::validate_feed_url($url)))
-				throw new SwiftRiver_Exception_ChannelOption('Invalid URL');
+				throw new Swiftriver_Exception_Channel_Option('Invalid URL');
 
 			$option_data['value'] = $feed['value'];
 			$option_data['title'] = $feed['title'];
@@ -116,7 +116,7 @@ class Rss_Init {
 			return;
 			
 		if ( ! Upload::type($file, array('xml')))
-			throw new SwiftRiver_Exception_ChannelOption('Invalid file type');
+			throw new Swiftriver_Exception_Channel_Option('Invalid file type');
 			
 		// Begin processing the feed
 		$importer = new Swiftriver_OPML_Import();

plugins/twitter/config/plugin.php
@@ -27,13 +27,13 @@ return array(
 				'label' => __('Keyword'),
 				'type' => 'text',
 				'values' => array(),
-				'placeholder' => 'E.g. Ushahidi, "African Tech"'
+				'placeholder' => 'E.g. Ushahidi, "African Tech" For multiple keywords, separate each keyword with a ","'
 			),
 			'user' => array(
 				'label' => __('User'),
 				'type' => 'text',
 				'values' => array(),
-				'placeholder' => 'E.g. @ushahidi, @dkobia'
+				'placeholder' => 'E.g. @ushahidi To add multiple users, seperate each user with a "," e.g. @ushahidi, @crowdmap'
 			)
 		),
 		'dependencies'	=> array(

plugins/twitter/init.php
@@ -10,6 +10,11 @@
  */
 class Twitter_Init {
 
+	/**
+	 * Twitter API endpoint for looking up screen names
+	 */
+	const LOOKUP_URL  = "https://api.twitter.com/1/users/show.json?screen_name=%s";
+
 	public function __construct()
 	{
 	    // Register as a crawler
@@ -51,7 +56,36 @@ class Twitter_Init {
 		// Apply validation rules to the options
 		if (isset($option_data['channel']) AND $option_data['channel'] == 'twitter')
 		{
-			// TODO: Sanity checks for the values
+			// Validate user ids - Verify that they exist on twitter
+			if ($option_data['key'] == 'user')
+			{
+				$screen_names = explode(",", str_replace('"', '', $option_data['value']));
+				
+				foreach ($screen_names as $screen_name)
+				{
+					// Strip the '@' off the screen name
+					if ('@' === substr($screen_name, 0, 1))
+					{
+						$screen_name = substr($screen_name, 1, strlen($screen_name) - 1);
+					}
+
+					$user_lookup_url = sprintf(Twitter_Init::LOOKUP_URL, $screen_name);
+					$request =  Request::factory($user_lookup_url);
+
+					// Execute the response
+					$response = Request_Client_Curl::factory()
+								    ->execute($request);
+
+					$response_array =  json_decode($response->body(), TRUE);
+					if (array_key_exists('error', $response_array))
+					{
+						$exception_message = __('Invalid twitter user - @:screen_name',
+							array(':screen_name' => $screen_name));
+						throw new Swiftriver_Exception_Channel_Option($exception_message);
+					}
+
+				}
+			}
 		}
 	}
 

themes/default/media/css/less/styles.less
@@ -1539,6 +1539,7 @@ article.container div.parameter input[type="text"],
 article.container div.parameter input[type="email"],
 article.container div.parameter input[type="password"] {
    width: 50%;
+   margin-left: 30px;
 }
 
 article.container div.parameter div.actions {
@@ -2477,6 +2478,12 @@ article.container section.property-parameters p.title {
    padding: 6px 5px 6px 0;
 }
 
+article.container section.property-parameters p.field-text {
+    float: left;
+    width: 50%;
+    padding: 6px 5px 6px 30px;
+    display: inline-block;
+}
 /* --------------------------- USER */
 
 #content.user section.property-parameters div.activity-item span.icon {

commit 40976c34711cc62721f26c3b22cff40da1e94dde

OPML import fixes
26134e23 broke:
* Channel option exception handling. Exception class renamed and all
references not updated.
* Loading icon display

application/classes/controller/river/channels.php
@@ -168,14 +168,14 @@ class Controller_River_Channels extends Controller_River_Settings {
 			echo json_encode(array('error' => __("Invalid file")));
 			return;
 		}
-		
+		sleep(5);
 		// Pass on the files to plugings
 		try 
 		{
 			$file['key'] = $this->request->post('key');
 			Swiftriver_Event::run('swiftriver.channel.option.file', $file);
 		}
-		catch (SwiftRiver_Exception_ChannelOption $e)
+		catch (SwiftRiver_Exception_Channel_Option $e)
 		{
 			$this->response->status(400);
 			$this->response->headers('Content-Type', 'application/json');
@@ -200,7 +200,7 @@ class Controller_River_Channels extends Controller_River_Settings {
 				$option_array = $this->river->get_channel_options($channel, $channel_option->id);
 				$options_array[] = $option_array[0];
 			}
-			catch (SwiftRiver_Exception_ChannelOption $e) 
+			catch (SwiftRiver_Exception_Channel_Option $e) 
 			{
 				// Do nothing
 			}

themes/default/views/pages/river/settings/channels.php
@@ -390,13 +390,13 @@ $(function() {
 					// Disable the inputs and show the loading icon
 					view.$("span.error-message").remove();
 					view.$("input").attr("disabled", "disabled").blur();
-					view.$("a.save").parents("p.actions").hide().after(loading_msg);
+					view.$(".button-blue").hide().after(loading_msg);
 				},
 				done: function (e, data) {
 					if (!data.result.length) {
 						var error_msg = $('<span class="error-message">No parameters were found in the file</span>');
 						loading_msg.replaceWith(error_msg).remove();
-						view.$("a.save").parents("p.actions").fadeIn();
+						view.$(".button-blue").fadeIn()
 						view.$("input").removeAttr("disabled");
 						return;
 					} else {
@@ -412,7 +412,7 @@ $(function() {
 					} 
 					var error_msg = $('<span class="error-message">' + message + '</span>');
 					loading_msg.replaceWith(error_msg).remove();
-					view.$("a.save").parents("p.actions").fadeIn()
+					 view.$(".button-blue").fadeIn()
 					view.$("input").removeAttr("disabled");
 				}
 			});