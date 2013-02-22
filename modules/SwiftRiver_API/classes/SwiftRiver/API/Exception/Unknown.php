edb1853b0a6b6f8cf9b03661083b61938e13fc82
Emmanuel Kala <emkala@gmail.com>
2013-02-22 15:12:30 +0300

    Fix class names
    * The exception classes were prefixed with "Swiftriver" instead of "SwiftRiver"
    This was causing Kohana to throw an ErrorException on *nix systems
    (case-sensitive)

--- modules/SwiftRiver_API/classes/SwiftRiver/API/Exception/Unknown.php
+++ modules/SwiftRiver_API/classes/SwiftRiver/API/Exception/Unknown.php
@@ -1,17 +1,17 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 /**
- * Api Error Exception
+ * API Error Exception
  *
  * PHP version 5
  * LICENSE: This source file is subject to the AGPL license 
  * that is available through the world-wide-web at the following URI:
  * http://www.gnu.org/licenses/agpl.html
  * @author      Ushahidi Team <team@ushahidi.com> 
- * @package    SwiftRiver - https://github.com/ushahidi/SwiftRiver
- * @subpackage  Exceptions
+ * @package     SwiftRiver - https://github.com/ushahidi/SwiftRiver
+ * @category    Exception
  * @copyright   Ushahidi - http://www.ushahidi.com
- * @license    http://www.gnu.org/licenses/agpl.html GNU Affero General Public License (AGPL)
+ * @license     http://www.gnu.org/licenses/agpl.html GNU Affero General Public License (AGPL)
  */
 
-class Swiftriver_API_Exception_Unknown extends Swiftriver_API_Exception {
+class SwiftRiver_API_Exception_Unknown extends SwiftRiver_API_Exception {
 }
